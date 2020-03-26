4a01bcb2412e3eab392e061329f14b0e96d2b37b
jeiner <tecjeinenr@gmail.com>
2020-03-26 01:04:32 -0500

    asd

--- app/app.py
+++ app/app.py
@@ -1,12 +1,26 @@
 # app
 
+import os
+
 from flask import Flask, render_template
+
+from flask import request
+
+from flask_sqlalchemy import SQLAlchemy
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "bookdatabase.db"))
 
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 
-@app.route("/")
+db = SQLAlchemy(app)
+
+@app.route("/", methods=["GET", "POST"])
 def hello():
+    if request.form:
+        print(request.form)
     return render_template("home.html")
 @app.route("/chacal")
 def saludos():
